7453d81cc756e1e443a7bb1ca4a5273df9a4bc43
e-turcotte <eturcotte@utexas.edu>
2023-05-30 22:59:47 -0500

    more experimentation with swing

--- sudokusolver/SudokuMain.java
+++ sudokusolver/SudokuMain.java
@@ -10,14 +10,35 @@ public class SudokuMain{
         f.setSize(400,500);
         f.setLayout(null);
 
-        JButton b = new JButton("click me uwu");
-        b.setBounds(130,100,100,40);
-        b.addActionListener(new ActionListener(){
+        JButton b1 = new JButton("click me uwu");
+        b1.setBounds(130,100,200,40);
+        b1.addActionListener(new ActionListener(){
+            @Override
             public void actionPerformed(ActionEvent e){
                 System.out.println("neat");
             }
         });
-        f.add(b);
+        f.add(b1);
+
+        JButton b2 = new JButton("also me");
+        b2.setBounds(0, 0, 150, 30);
+        b2.addActionListener(new ActionListener(){
+            @Override
+            public void actionPerformed(ActionEvent e){
+                System.out.println("cool");
+            }
+        });
+        f.add(b2);
+
+        JButton b3 = new JButton("helper");
+        b3.setBounds(150, 30, 150, 30);
+        b3.addActionListener(new ActionListener(){
+            @Override
+            public void actionPerformed(ActionEvent e){
+                SudokuCmd.help();
+            }
+        });
+        f.add(b3);
 
         f.setVisible(true);
     }
